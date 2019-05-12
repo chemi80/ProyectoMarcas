ffac86cd3f6708273c8e118963864b4f755007b1
chemi80 <jmmorangallego@gmail.com>
2019-05-12 03:34:55 +0200

    ProyectoMarcas1DAW

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btnmenu = document.getElementById('btnmenu');
+var menu = document.getElementById('menu');
+btnmenu.addEventListener('click', function(){
+	'use strict';
+	menu.classList.toggle('mostrar');
+});
